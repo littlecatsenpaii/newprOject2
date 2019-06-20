3a33f163aa9b4ea84a89857da67ab50b5588c931
littlecatsenpaii <51970260+littlecatsenpaii@users.noreply.github.com>
2019-06-20 13:08:29 -0500

    When you want to bake a cake

---
+++ grados_f_a_c.py
@@ -0,0 +1,5 @@
+grados_en_f = int(input("Introduce the °F that you want to change (write only the number): "))
+
+grados_en_c = (grados_en_f - 32) / 1.8
+
+print("Your {}°F are {}°C".format(grados_en_f, grados_en_c))
