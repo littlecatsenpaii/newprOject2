f3540eb656df255795762485428aaf83547ac9a8
littlecatsenpaii <51970260+littlecatsenpaii@users.noreply.github.com>
2019-06-20 13:07:54 -0500

    Super easy

---
+++ Guess the number_ 2.0.py
@@ -0,0 +1,21 @@
+numadivinar = input("Choose a number to guess it from 1 to 10: ")
+
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+
+numero_dos = input("Guess the number: ")
+
+while numero_dos != numadivinar:
+        numero_dos = input("Guess the number: ")
+print("The number was {}".format(numadivinar))
+print("You guessed the number, yeii ")
+
+
